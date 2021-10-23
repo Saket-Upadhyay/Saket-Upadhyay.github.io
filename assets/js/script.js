86a8c9c5aa2d05896fcba29481db7d751606c3c3
Saket Upadhyay <saketupadhya@gmail.com>
2021-10-24 00:08:24 +0530

    adding about and inspiration and research

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,30 @@
 $( document ).ready(function() {
-                
-                $("#about_scroll").fadeOut();   
+                // For Debugging
+                // $('#index').fadeOut();
+                $("#about_scroll").fadeOut()
+                $("#inspire_scroll").fadeOut();  ;   
                 $("#reseach_scroll").fadeOut();
                 $("#projects_scroll").fadeOut();
+                $("#patent_scroll").fadeOut();
 
                 $("#about").click(function(){
                     $("#index").fadeOut();
                     $("#about_scroll").fadeIn();
                     $('#about_left').addClass('animated slideInLeft');
                     $('#about_right').addClass('animated slideInRight');
+                    $("#inspire_scroll").fadeIn();
+                    $('#inspire_left').addClass('animated slideInLeft');
+                    $('#inspire_right').addClass('animated slideInRight');
+
                     });
                 $("#reseach").click(function(){
                     $("#index").fadeOut();
                     $("#reseach_scroll").fadeIn();
                     $('#reseach_left').addClass('animated slideInLeft');
                     $('#reseach_right').addClass('animated slideInRight');
+                    $("#patent_scroll").fadeIn();
+                    $('#patent_left').addClass('animated slideInLeft');
+                    $('#patent_right').addClass('animated slideInRight');
                     });
                 $("#projects").click(function(){
                     $("#index").fadeOut();
